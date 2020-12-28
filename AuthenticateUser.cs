f92e45a0c3fd130842406c74ddad18d79fc6c8ef
crystal / ue <35664551+byeue@users.noreply.github.com>
2020-12-27 23:59:13 -0500

    Add files via upload

---
+++ AuthenticateUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class AuthenticateUser : MessageBase
+{
+    public string username;
+    public string password;
+    public string ipAddress;
+    public int requestCode;
+    //public string connectionId;
+    //public string ipAddress;
+
+
+
+}
